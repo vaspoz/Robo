cccc7f1c413f957107f6adc05e668a7d6345a98b
vaspoz <vaspoz@mail.ru>
2017-10-02 15:58:22 +0200

    FileManager suppose to save and load objects from the system

---
+++ src/main/java/utils/FileManager.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.io.*;
+
+/**
+ * Created by Vasilii_Pozdeev on 2/10/2017.
+ */
+public class FileManager {
+    public static void saveMacro(String fileName, Pairs mousePoints) {
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(fileName + ".mcr"));
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(mousePoints);
+
+            oos.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Pairs loadMacro(String macroName) {
+        try {
+            FileInputStream fis = new FileInputStream(new File(macroName + ".mcr"));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Pairs loadedPairs = (Pairs)ois.readObject();
+            return loadedPairs;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
