240332fd22e5d6883fec977912001f723cba2692
vaspoz <vaspoz@mail.ru>
2017-10-06 22:01:48 +0200

    Order should be determined by insertion

--- src/main/java/utils/UserActions.java
+++ src/main/java/utils/UserActions.java
@@ -14,14 +14,9 @@ public class UserActions implements Serializable {
     public static final long serialVersionUID = 2L;
 
     private Map<Point, String> pointerAction;
-    private Pairs mousePointers;
-    private List<String> actions;
 
     public UserActions() {
-        pointerAction = new HashMap<>();
-
-        mousePointers = new Pairs();
-        actions = new ArrayList<>();
+        pointerAction = new LinkedHashMap<>();
     }
 
     public void setActionForPoint(Point point, String action) {
@@ -39,20 +34,6 @@ public class UserActions implements Serializable {
     public boolean containPointer(Point point) {
         return pointerAction.containsKey(point);
 
-    }
-
-//    public double[] getMousePointer(int i) {
-//        return mousePointers.get(i);
-//    }
-
-    public void assignActions(Scanner sc) {
-        for (int i = 0; i < mousePointers.getSize(); i++) {
-            double[] point = mousePointers.get(i);
-            System.out.print("Enter action for point [" + point[0] + ", " + point[1] + "]: ");
-            String action = sc.nextLine();
-
-            actions.add(i, action);
-        }
     }
 
     public void addMousePointer(Point mousePoint) {
